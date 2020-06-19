879726f6b970c1d386f0cd07090b3ad8388bc324
user <user@adminde-MacBook-Pro-2.local>
2020-06-19 18:36:01 +0800

    篩選功能(未完成)

--- api/add.php
+++ api/add.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
  
 
@@ -9,13 +9,16 @@ switch (true) {
     case (!isset($_POST['name'])):
     case (!isset($_POST['tel'])):
     case (!isset($_POST['email'])):
+        print_r($_POST);
+        print_r($_GET);
         echo json_encode(["code" => 400, "data" => [], "error" => "缺少參數"], true);
         exit();
 }
 $str = file_get_contents($fname);
 $newStr = "" ;
 if (strlen($str) != 0)  $newStr .= "\r\n";
-$newStr .= time() . rand(1, 999) . "|" . $_POST['name'] . "|" . $_POST['tel'] . "|" . $_POST['email'];
+$id = time() . rand(1, 999);
+$newStr .=  $id . "|" . $_POST['name'] . "|" . $_POST['tel'] . "|" . $_POST['email'];
 // $newStr .= rand(1, 1000000) . "|dasiy|2885252|dasiy@gmail.gg";
 
 file_put_contents("test.txt", "$newStr", FILE_APPEND);
@@ -26,4 +29,5 @@ foreach ($str as $k => $v) {
     $tmp[] =  explode("|", $v);
 }
 
-echo json_encode(["code" => 200, "data" => $tmp], true);
+echo json_encode(["code" => 200, "data" => ["id" => $id ] ], true);
+// echo json_encode(["code" => 200, "data" => $tmp], true);
